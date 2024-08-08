72e1cd9d3a2e012856852e806736223585697e4f
Pablo Prata <pabloalmeidaprata@hotmail.com>
2024-08-08 10:42:14 -0300

    FIX: @Transactional em BeneficiarioServiceImpl

--- src/main/java/org/kan/kancare/boot/service/BeneficiarioServiceImpl.java
+++ src/main/java/org/kan/kancare/boot/service/BeneficiarioServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Service
+@Service @Transactional
 public class BeneficiarioServiceImpl implements BeneficiarioService {
 
     @Autowired
