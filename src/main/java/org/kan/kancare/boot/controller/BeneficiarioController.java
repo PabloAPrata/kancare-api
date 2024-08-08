2e1f5d42d42753e8928f0ea76d37f848d9b87aea
Pablo Prata <pabloalmeidaprata@hotmail.com>
2024-08-08 02:18:55 -0300

    FIX: Funcionalidades: cadastrar e listar documentos de beneficiario

--- src/main/java/org/kan/kancare/boot/controller/BeneficiarioController.java
+++ src/main/java/org/kan/kancare/boot/controller/BeneficiarioController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
+@SuppressWarnings("unused")
 @RestController
 @RequestMapping("beneficiarios")
 public class BeneficiarioController {
